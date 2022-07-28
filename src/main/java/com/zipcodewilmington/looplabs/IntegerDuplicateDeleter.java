a196f84b3782c2849cfecd99ce17e2a52626a4b8
eric <ericarterbridge@gmail.com>
2022-07-28 16:34:41 -0400

    passed all tests

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,79 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    Integer[] intArray;
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+        this.intArray = intArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        List<Integer> newList = new ArrayList<>();
+        List<Integer> removedList = new ArrayList<>();
+        newList.addAll(Arrays.asList(intArray));
+        int count = 0;
+        int totalCount = 0;
+        for (Integer element : intArray){
+            count = 0;
+            for (Integer element2 : intArray){
+                if (Objects.equals(element, element2)){
+                    count++;
+                }
+                }
+            if (count >= maxNumberOfDuplications){
+                removedList.add(element);
+                totalCount += count;
+            }
+        }
+        for (Integer element : intArray){
+            for (Integer element2 : removedList){
+                if (Objects.equals(element, element2)){
+                    newList.remove(element);
+                }
+            }
+        }
+        Integer[] newArr = new Integer[newList.size()];
+        newArr = newList.toArray(newArr);
+        return newArr;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<Integer> newList = new ArrayList<>();
+        List<Integer> removedList = new ArrayList<>();
+        newList.addAll(Arrays.asList(intArray));
+        Integer count = 0;
+        for (Integer element : intArray){
+            count = 0;
+            for (Integer element2 : intArray){
+                if (Objects.equals(element, element2)){
+                    count++;
+                }
+
+                }
+            if (count == exactNumberOfDuplications){
+                removedList.add(element);
+            }
+        }
+        for (Integer element : intArray){
+            for (Integer element2 : removedList){
+                if (Objects.equals(element, element2)){
+                    newList.remove(element);
+                }
+            }
+        }
+        Integer[] newArr = new Integer[newList.size()];
+        newArr = newList.toArray(newArr);
+        return newArr;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,78 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    String[] intArray;
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+        this.intArray = intArray;
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        List<String> newList = new ArrayList<>();
+        List<String> removedList = new ArrayList<>();
+        newList.addAll(Arrays.asList(intArray));
+        Integer count = 0;
+        for (String element : intArray){
+            count = 0;
+            for (String element2 : intArray){
+                if (Objects.equals(element, element2)){
+                    count++;
+                }
+            }
+            if (count >= maxNumberOfDuplications){
+                removedList.add(element);
+            }
+        }
+        for (String element : intArray){
+            for (String element2 : removedList){
+                if (Objects.equals(element, element2)){
+                    newList.remove(element);
+                }
+            }
+        }
+        String[] newArr = new String[newList.size()];
+        newArr = newList.toArray(newArr);
+        return newArr;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<String> newList = new ArrayList<>();
+        List<String> removedList = new ArrayList<>();
+        newList.addAll(Arrays.asList(intArray));
+        Integer count = 0;
+        for (String element : intArray){
+            count = 0;
+            for (String element2 : intArray){
+                if (Objects.equals(element, element2)){
+                    count++;
+                }
+
+            }
+            if (count == exactNumberOfDuplications){
+                removedList.add(element);
+            }
+        }
+        for (String element : intArray){
+            for (String element2 : removedList){
+                if (Objects.equals(element, element2)){
+                    newList.remove(element);
+                }
+            }
+        }
+        String[] newArr = new String[newList.size()];
+        newArr = newList.toArray(newArr);
+        return newArr;
+    }
 }
